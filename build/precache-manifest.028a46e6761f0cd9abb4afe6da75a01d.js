66ce75a30d3e9ec0d24966cb42f879c3274c8cbd
wilf@wilfhorsfall.com <wilf@wilfhorsfall.com>
2025-04-13 15:19:42 +0100

    transfer

---
+++ build/precache-manifest.028a46e6761f0cd9abb4afe6da75a01d.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "613a40be6bcc5a5bf75f",
+    "url": "/static/css/main.19cc1e64.chunk.css"
+  },
+  {
+    "revision": "613a40be6bcc5a5bf75f",
+    "url": "/static/js/main.613a40be.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "86c5668f3ac820074e7b",
+    "url": "/static/js/2.86c5668f.chunk.js"
+  },
+  {
+    "revision": "7fc7e388a8ff0d13c3e02f2d980aff2d",
+    "url": "/static/media/arrow.7fc7e388.svg"
+  },
+  {
+    "revision": "05bf55bf973b12c74c7d651462f60016",
+    "url": "/static/media/brokencloudday.05bf55bf.jpg"
+  },
+  {
+    "revision": "9dd020621321883e3e0f71bc0111db03",
+    "url": "/static/media/fewcloudnight.9dd02062.jpg"
+  },
+  {
+    "revision": "2776c43b331757707281c649879f671d",
+    "url": "/static/media/showerday.2776c43b.jpg"
+  },
+  {
+    "revision": "31a7e6a5d7ed517b020c2323c3c158cf",
+    "url": "/static/media/mistday.31a7e6a5.jpg"
+  },
+  {
+    "revision": "9401658263b74f9f7b2981157b736175",
+    "url": "/static/media/fewclouds.94016582.jpg"
+  },
+  {
+    "revision": "b143d7722b61384913e2573f40dccb2c",
+    "url": "/static/media/snowday.b143d772.jpg"
+  },
+  {
+    "revision": "9913965a59d010739f07c14b7a3ea978",
+    "url": "/static/media/cloudnight.9913965a.jpg"
+  },
+  {
+    "revision": "d2df3f35ecf7f98ebe1c91fdcf2df404",
+    "url": "/static/media/brokencloudnight.d2df3f35.jpg"
+  },
+  {
+    "revision": "db7e1bad9a67d8741886a41063a2f726",
+    "url": "/static/media/thunderday.db7e1bad.jpg"
+  },
+  {
+    "revision": "beed6ae3951485d54e40dfd700f769bc",
+    "url": "/static/media/thundernight.beed6ae3.jpg"
+  },
+  {
+    "revision": "af284c5ef7cef1bd8f1a9a4b259685c5",
+    "url": "/static/media/cloudday.af284c5e.jpg"
+  },
+  {
+    "revision": "3a4cd1669b1bbd8c8f78305e2104687c",
+    "url": "/static/media/clearnight.3a4cd166.jpg"
+  },
+  {
+    "revision": "6895647d6bb6eca545f05311b8abfbf4",
+    "url": "/static/media/snownight.6895647d.jpg"
+  },
+  {
+    "revision": "c0b16d7dcf79af576d611d5d4fc25fbb",
+    "url": "/static/media/mistnight.c0b16d7d.jpg"
+  },
+  {
+    "revision": "5fbf97f68f142d78044629d7de3496ef",
+    "url": "/static/media/rainnight.5fbf97f6.jpg"
+  },
+  {
+    "revision": "712f9e0263cea9c555315a67b505b5e5",
+    "url": "/static/media/showernight.712f9e02.jpg"
+  },
+  {
+    "revision": "4231e9056a06523c76f48def0081b97f",
+    "url": "/static/media/rainday.4231e905.jpg"
+  },
+  {
+    "revision": "f223e27aa3e1b00af7d393013b4b8915",
+    "url": "/static/media/sun.f223e27a.jpg"
+  },
+  {
+    "revision": "e8d634ba9991bc134b349d6d141c3143",
+    "url": "/static/media/leftarrow.e8d634ba.svg"
+  },
+  {
+    "revision": "c4fef3e8cd71ad83b32d5be583a410ba",
+    "url": "/static/media/rightarrow.c4fef3e8.svg"
+  },
+  {
+    "revision": "474f08ce5778a85246028973d8c45aa6",
+    "url": "/static/media/reddit.474f08ce.svg"
+  },
+  {
+    "revision": "16eefbc0bff5d929faa24960bc08702c",
+    "url": "/static/media/facebook.16eefbc0.svg"
+  },
+  {
+    "revision": "424e91a635d0c8c5c4ca14a1c96bfc19",
+    "url": "/static/media/twitter.424e91a6.svg"
+  },
+  {
+    "revision": "ddfcc2f9a609a9d561c7ab59cd8e5ca1",
+    "url": "/static/media/downarrow.ddfcc2f9.svg"
+  },
+  {
+    "revision": "94b29d8640998e08bdfa55fda1b7cca4",
+    "url": "/index.html"
+  }
+];
---
+++ src/key.js
@@ -0,0 +1,2 @@
+export const Api_Key = "8c5021109e1918aa1c478ce6b4f53c27";
+export const Analytics_ID = "UA-136012452-1"
